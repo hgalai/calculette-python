27b144e1dde14bc97a8528badcf32722e6af1e64
hgalai <40334673+hgalai@users.noreply.github.com>
2019-12-19 04:07:02 +0100

    Add files via upload
    Version 2 améliorée

--- caculette.py
+++ caculette.py
@@ -1,31 +1,28 @@
 # -*-coding:Latin-1 -*
-
+# -*-coding:utf-8 -*
 def add(x,y):
     z=x+y
-    print(z)
+    print("le résultat est: ",z)
 def soust(x,y):
     z=x-y
-    print(z)
+    print("le résultat est: ",z)
 def mult(x,y):
     z=x*y
-    print (z)
+    print ("le résultat est: ",z)
 def div(x,y):
     z=x/y
-    print(z)
+    print("le résultat est: ",z)
 
-#print("Calculatrice")
+print("Calculatrice")
 a=input("Choisissez une opération: 'add', 'soust', 'mult', 'div' :")
-b= input("donnez un premier nombre:")
-b=float(b)
-print(type(b))
-c= input("donnez un second nombre:")
-c=float(c)
+b= float(input("donnez un premier nombre:"))
+c= float(input("donnez un second nombre:"))
 if a=='add':
     add(b,c)
-if a== 'soust':
+if a=='soust':
     soust(b,c)
-if a== 'mult':
+if a=='mult':
     mult(b,c)
-if a== 'div' :
+if a=='div' :
     div(b,c)
 
